85b54590dfc58a1f8c67a535b7ce1862603acc07
Mike Meyer <mike.meyer86@web.de>
2023-08-21 20:49:14 +0200

    error on new chat dialog fixed

--- src/app/components/dialog-create-new-chat/dialog-create-new-chat.component.ts
+++ src/app/components/dialog-create-new-chat/dialog-create-new-chat.component.ts
@@ -7,17 +7,18 @@ import { UsersService } from 'src/app/services/users.service';
 @Component({
   selector: 'app-dialog-create-new-chat',
   templateUrl: './dialog-create-new-chat.component.html',
-  styleUrls: ['./dialog-create-new-chat.component.scss']
+  styleUrls: ['./dialog-create-new-chat.component.scss'],
 })
 export class DialogCreateNewChatComponent {
   dmForm: FormGroup;
   userList: any[];
   filteredUsers: any[];
   isButtonDisabled: boolean = true;
+  inputTouched: boolean = false;
 
   /**
    * Constructor function
-   * 
+   *
    * @param formBuilder - FormBuilder instance used to create the form group.
    * @param usersService - UsersService instance used to retrieve the user list.
    */
@@ -25,24 +26,24 @@ export class DialogCreateNewChatComponent {
     private formBuilder: FormBuilder,
     private usersService: UsersService,
     private chatService: ChatService,
-    private dialogRef: MatDialogRef<DialogCreateNewChatComponent>) {
-      
+    private dialogRef: MatDialogRef<DialogCreateNewChatComponent>
+  ) {
     // Create the form group using the formBuilder
     this.dmForm = this.formBuilder.group({
-      displayName: ''
+      displayName: '',
     });
 
-     // Retrieve the user list from the usersService and deletes the currentUser from it.
+    // Retrieve the user list from the usersService and deletes the currentUser from it.
     this.userList = this.usersService.currentUsers;
     let currentUser = this.usersService.currentUserId$;
-    this.userList = this.userList.filter(user => user.uid !== currentUser);
+    this.userList = this.userList.filter((user) => user.uid !== currentUser);
     // initialize the filteredUsers array with the user list
     this.filteredUsers = this.userList;
   }
 
   /**
    * Method for selecting a user from the autocomplete list.
-   * 
+   *
    * @param user - The selected user object.
    */
   selectUser(user: any) {
@@ -53,21 +54,27 @@ export class DialogCreateNewChatComponent {
 
   /**
    * Method for filtering the user list based on the input value.
-   * 
+   *
    */
   filterUsers() {
     const searchValue = this.dmForm.value.displayName.toLowerCase();
-    this.filteredUsers = this.userList.filter(user => {
+    this.filteredUsers = this.userList.filter((user) => {
       const displayNameLower = user.displayName.toLowerCase();
-      return displayNameLower.includes(searchValue) && displayNameLower.indexOf(searchValue) === 0;
+      return (
+        displayNameLower.includes(searchValue) &&
+        displayNameLower.indexOf(searchValue) === 0
+      );
     });
 
-    this.isButtonDisabled = !this.filteredUsers.some(user => user.displayName.toLowerCase() == searchValue);
+    this.isButtonDisabled = !this.filteredUsers.some(
+      (user) => user.displayName.toLowerCase() == searchValue
+    );
+    this.inputTouched = true;
   }
 
   /**
    * OnSubmit Method. Opens or creates a chat with the selected user depending if there is already an existing one.
-   * 
+   *
    */
   onSubmit() {
     this.chatService.openChat(this.dmForm.value.uid);
